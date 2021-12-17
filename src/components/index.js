f04fed218c0fe746122742779fa18ba32ba1c75c
federicocanesa <federicocanesa@gmail.com>
2021-12-17 17:47:44 -0300

    Se crea index en componentes

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Copyright from './Copyright/Copyright';
+import Footer from './Footer/Footer';
+import Menu from './Menu/Menu';
+import Table from './Table/Table';
+import Error from './Error/Error';
+import Layout from './Layout/Layout';
+
+export {
+    Copyright,
+    Footer,
+    Menu,
+    Table,
+    Error,
+    Layout
+}
